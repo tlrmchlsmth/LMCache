5c0b89e5c8dd3bbae460cda64ffb8ed36db3879d
Or Ozeri <oro@il.ibm.com>
2025-04-24 20:45:55 +0300

    p2p: disable distributed server
    Currently, the p2p distributed server is broken in vllm v1.
    This commit disables it for now.
    
    Signed-off-by: Or Ozeri <oro@il.ibm.com>

--- lmcache/experimental/cache_engine.py
+++ lmcache/experimental/cache_engine.py
@@ -96,7 +96,7 @@ class LMCacheEngine:
             self.storage_manager = StorageManager(
                 config, metadata, self.memory_allocator,
                 self.lookup_server)  # type: ignore[assignment]
-        if self.enable_p2p:
+        if self.enable_p2p and False:
             self.distributed_loop = asyncio.get_event_loop()
             assert self.lookup_server is not None
             assert isinstance(self.storage_manager, StorageManager)
@@ -285,7 +285,7 @@ class LMCacheEngine:
             memory_obj = self.storage_manager.get(key)
 
             if memory_obj is None:
-                if self.enable_p2p:
+                if self.enable_p2p and False:
                     future_memory_obj = asyncio.run_coroutine_threadsafe(
                         self.distributed_server.issue_get(key),
                         self.distributed_loop)
@@ -368,7 +368,7 @@ class LMCacheEngine:
     def close(self) -> None:
         """Close the cache engine and free all the resources"""
 
-        if self.enable_p2p:
+        if self.enable_p2p and False:
             self.distributed_server.close()
 
         if self.config.enable_controller:
